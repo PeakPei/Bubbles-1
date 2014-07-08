d73f25c4856358e1f4f541ea9641e44f45964334
Sophia Anopa <anopa@fb.com>
2014-07-08 10:28:24 -0700

    Updated playerCircle to move when touched again

--- Bubbles/STSGameScene.m
+++ Bubbles/STSGameScene.m
@@ -10,6 +10,7 @@
 
 @interface STSGameScene ()
 @property BOOL contentCreated;
+@property BOOL circleSelected;
 @end
 
 @implementation STSGameScene
@@ -18,6 +19,9 @@
     if (!self.contentCreated) {
         [self createSceneContents];
         self.contentCreated = YES;
+        UIPanGestureRecognizer *gestureRecogniser = [[UIPanGestureRecognizer alloc] initWithTarget:self
+                                                                                            action:@selector(flicked:)];
+        [view addGestureRecognizer:gestureRecogniser];
     }
 }
 
@@ -31,7 +35,59 @@
     self.playerCircle.physicsBody.dynamic = YES;
     self.playerCircle.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
     self.physicsWorld.gravity = CGVectorMake(0.0,0.0);
+    
     [self addChild:self.playerCircle];
     
+    //self.playerCircle.userInteractionEnabled = YES;
+    
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.playerCircle removeAllActions];
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    //self.circleSelected = NO;
+    UITouch *touch = [touches anyObject];
+    CGPoint location = [touch locationInView:self.view];
+    location.y = self.view.frame.size.height - location.y;
+    NSMutableArray *nodes = [self nodesAtPoint:location];
+    for (SKSpriteNode *node in nodes)
+    {
+        if ([self.playerCircle isEqual:node])
+        {
+            self.circleSelected = YES;
+            NSLog(@"circle selected");
+        }
+        //[self.playerCircle runAction:[SKAction repeatActionForever:UI]]
+    }
+}
+
+
+
+- (void)flicked:(UIPanGestureRecognizer *)gr
+{
+    if(self.circleSelected)
+    {
+        if (gr.state == UIGestureRecognizerStateChanged)
+        {
+            CGPoint move = [gr translationInView:self.view];
+            SKAction *moveAction = [SKAction moveByX:move.x
+                                                   y:-move.y
+                                            duration:0];
+            [self.playerCircle runAction:moveAction];
+            [gr setTranslation:CGPointMake(0, 0)
+                        inView:self.view];
+        }
+        
+        if(gr.state == UIGestureRecognizerStateEnded)
+        {
+            CGPoint velocity = [gr velocityInView:self.view];
+            velocity = [self convertPointFromView:velocity];
+            
+            [self.playerCircle.physicsBody applyForce:CGVectorMake(velocity.x, velocity.y)];
+            self.circleSelected = NO;
+            
+        }
+    }
 }
 @end
