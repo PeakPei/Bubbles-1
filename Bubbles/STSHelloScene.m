1c9b1c962638701263b6d09acf41ab8e4dc76eaf
Sophia Anopa <anopa@fb.com>
2014-07-09 10:37:57 -0700

    Added “Bubbles” Title

--- Bubbles/STSHelloScene.m
+++ Bubbles/STSHelloScene.m
@@ -29,6 +29,43 @@
     self.scaleMode = SKSceneScaleModeAspectFit;
     [self addChild:[self newGameNode]];
     [self addChild:[self newDerpyNode]];
+    
+    //TITLE
+    [self addChild:[self newTitleNode]];
+}
+
+- (SKSpriteNode *)newTitleNode
+{
+    
+    SKLabelNode *titleNode = [SKLabelNode labelNodeWithFontNamed:@"Helvetica-Bold"];
+    titleNode.fontColor = [UIColor colorWithRed:131/255.0 green:159/255.0 blue:203/255.0 alpha:0.9];
+    titleNode.text = @"Bubbles";
+    titleNode.fontSize = 50;
+    titleNode.position = CGPointMake(self.frame.size.width * 1/2, self.frame.size.height * 5/7);
+
+    
+    SKSpriteNode *bg = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:59/255.0 green:89/255.0 blue:152/255.0 alpha:1.0] size:CGSizeMake(titleNode.frame.size.width + 20, titleNode.frame.size.height + 80)];
+    [bg initWithImageNamed:[NSString stringWithFormat:@"bubbles.png"]];
+    bg.size = CGSizeMake(titleNode.frame.size.width + 20, titleNode.frame.size.height +80);
+    bg.position = titleNode.position;
+    titleNode.position = CGPointMake((bg.frame.size.width - titleNode.frame.size.width)/2 - 10, -(bg.frame.size.height - titleNode.frame.size.height)/2);
+    [bg addChild:titleNode];
+    bg.name = @"titleNode";
+    
+    //Floating effect
+    titleNode.physicsBody.dynamic = YES;
+    SKAction *moveUp = [SKAction moveTo:CGPointMake(titleNode.position.x, titleNode.position.y-5)
+                               duration:1.0];
+    SKAction *moveDown = [SKAction moveTo:CGPointMake(titleNode.position.x, titleNode.position.y+40)
+                                 duration:1.0];
+    SKAction *move = [SKAction sequence:@[moveUp, moveDown]];
+    move = [SKAction repeatActionForever:move];
+    [titleNode runAction:move];
+    
+    
+    
+    return bg;
+
 }
 
 - (SKSpriteNode *)newGameNode
@@ -39,14 +76,14 @@
     helloNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     helloNode.name = @"helloNode";
     return helloNode; */
-    SKLabelNode *gameNode = [SKLabelNode labelNodeWithFontNamed:@"ChalkboardSE-Bold"];
+    SKLabelNode *gameNode = [SKLabelNode labelNodeWithFontNamed:@"Helvetica-Bold"];
     gameNode.text = @"Normal Game";
-    gameNode.fontColor = [SKColor grayColor];
+    gameNode.fontColor = [SKColor whiteColor];
     gameNode.fontSize = 20;
-    gameNode.position = CGPointMake(self.frame.size.width * 1/2, self.frame.size.height * 2/3);
+    gameNode.position = CGPointMake(self.frame.size.width * 1/2, self.frame.size.height * 2/5);
 //    gameNode.name = @"gameNode";
     
-    SKSpriteNode *bg = [SKSpriteNode spriteNodeWithColor:[UIColor blueColor] size:CGSizeMake(gameNode.frame.size.width+20, gameNode.frame.size.height+20)];
+    SKSpriteNode *bg = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:59/255.0 green:89/255.0 blue:152/255.0 alpha:1.0] size:CGSizeMake(gameNode.frame.size.width+20, gameNode.frame.size.height+20)];
     bg.position = gameNode.position;
     gameNode.position = CGPointMake((bg.frame.size.width-gameNode.frame.size.width)/2 - 10, -(bg.frame.size.height - gameNode.frame.size.height)/2);
     [bg addChild:gameNode];
@@ -58,18 +95,21 @@
 
 -(SKSpriteNode *)newDerpyNode
 {
-    SKLabelNode *derpyNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    derpyNode.fontColor = [SKColor grayColor];
+    SKLabelNode *derpyNode = [SKLabelNode labelNodeWithFontNamed:@"Helvetica-Bold"];
+    derpyNode.fontColor = [SKColor whiteColor];
     derpyNode.text = @"Abnormal/Derpy Game";
     derpyNode.fontSize = 20;
-    derpyNode.position = CGPointMake(self.frame.size.width * 1/2, self.frame.size.height * 1/3);
+    derpyNode.position = CGPointMake(self.frame.size.width * 1/2, self.frame.size.height * 1/4);
 //    derpyNode.name = @"derpyNode";
     
-    SKSpriteNode *bg = [SKSpriteNode spriteNodeWithColor:[UIColor redColor] size:CGSizeMake(derpyNode.frame.size.width + 20, derpyNode.frame.size.height +20)];
+    SKSpriteNode *bg = [SKSpriteNode spriteNodeWithColor:[UIColor colorWithRed:59/255.0 green:89/255.0 blue:152/255.0 alpha:1.0] size:CGSizeMake(derpyNode.frame.size.width + 20, derpyNode.frame.size.height +20)];
+    [bg initWithImageNamed:[NSString stringWithFormat:@"rainbow.png"]];
+    bg.size = CGSizeMake(derpyNode.frame.size.width + 20, derpyNode.frame.size.height +20);
     bg.position = derpyNode.position;
     derpyNode.position = CGPointMake((bg.frame.size.width - derpyNode.frame.size.width)/2 - 10, -(bg.frame.size.height - derpyNode.frame.size.height)/2);
     [bg addChild:derpyNode];
     bg.name = @"derpyNode";
+    
     
 
     return bg;
